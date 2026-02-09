40b1573cce568e4eea63f084750e9de82fb149ba
Kumar <2463661@cognizant.com>
2026-02-09 11:31:31 +0530

    Resolve wanted properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { EmployeeComponent } from "../employee/employee.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeeComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
