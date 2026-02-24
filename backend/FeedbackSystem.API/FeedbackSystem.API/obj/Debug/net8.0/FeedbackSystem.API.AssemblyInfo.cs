7be900fcbe9ff80bfe5e83180d82e07aabe63f41
Kumar <2463661@cognizant.com>
2026-02-24 17:43:26 +0530

    add

--- backend/FeedbackSystem.API/FeedbackSystem.API/obj/Debug/net8.0/FeedbackSystem.API.AssemblyInfo.cs
+++ backend/FeedbackSystem.API/FeedbackSystem.API/obj/Debug/net8.0/FeedbackSystem.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FeedbackSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ec99e9fc71a422abb90424987ef22ab322cac69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70f5896397ba09f088aadb583b5e84c3f51b912e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FeedbackSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FeedbackSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
